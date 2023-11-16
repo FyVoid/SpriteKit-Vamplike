0ba3a4a02993f55e87a4c0993658a79df2968ca4
FyVoid <mythstudiofyvoid@outlook.com>
2023-11-16 18:41:23 +0800

    Initial Commit

---
+++ SwiftGame/SwiftGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGameApp.swift
+//  SwiftGame
+//
+//  Created by FyVoid F on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
