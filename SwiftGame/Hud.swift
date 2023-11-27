f039a41cb0ae537e795064ac4125e560d934390c
FyVoid <mythstudiofyvoid@outlook.com>
2023-11-27 11:19:57 +0800

    更好的hud

---
+++ SwiftGame/Hud.swift
@@ -0,0 +1,18 @@
+//
+//  Hud.swift
+//  SwiftGame
+//
+//  Created by FyVoid F on 2023/11/27.
+//
+
+import SwiftUI
+
+struct Hud: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hud()
+}
